ebd8380e26fdc2abae0f629aa35ec4f71d260fd0
DJ <123333887+JSDWRLD@users.noreply.github.com>
2023-05-14 18:38:53 -0700

    Map now has pins with rating and cards on left side for user

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+import { getPlacesData } from './api';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
 
 const App = () => {
+    const [places, setPlaces] = useState([]);
+    const [childClicked, setChildClicked] = useState(null);
+
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState({});
+
+    // use effect is always a callback function
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: {latitude, longitude}}) => {
+            setCoordinates({lat: latitude, lng: longitude});
+        })
+    }, []);
+
+    useEffect (() => {
+        getPlacesData(bounds.sw, bounds.ne)
+            .then((data) => {
+                //console.log(data);
+                setPlaces(data);
+            })
+    }, [coordinates, bounds]);
+
     return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing = {3} style = {{width: '100%'}}>
+                {/* makes sure that the map takes the majority of the page */}
+                <Grid item xs={12} md={4}>
+                    <List 
+                        places={places} 
+                        childClicked={childClicked}
+                    />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map 
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                        places={places}
+                        setChildClicked={setChildClicked}
+                    />
+                </Grid>
+            </Grid>
+        </>
     );
 }
 
